cbb6e333d4844c7bb731ed56cf97f4579d778f99
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-06-12 10:40:17 -0300

    fix: update nix, added update script
    now it's possible to run
    
    ```sh
    nix develop -c update
    ```
    
    and it should take care of everything.

--- flake.nix
+++ flake.nix
@@ -2,25 +2,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    toolbox.url = "github:catppuccin/toolbox";
+    whiskers.url = "github:catppuccin/whiskers";
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-    toolbox,
-  } @ inputs:
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    , whiskers
+    ,
+    } @ inputs:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-        whiskers = inputs.toolbox.packages.${system}.whiskers;
-      in {
+        whiskers = inputs.whiskers.packages.${system}.default;
+      in
+      {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             go
             gopls
             whiskers
+            (writeScriptBin "update" ''
+              ${whiskers}/bin/whiskers go.tera
+              go fmt
+            '')
           ];
         };
       }
